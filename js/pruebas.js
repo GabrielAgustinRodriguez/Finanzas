ba1c2c5571b6a8ba5444bea059ec21a1d046029e
GabrielAgustinRodriguez <gabriel_a_rodriguez96@hotmail.com>
2023-05-07 20:51:46 -0300

    Conversiones
    Carga de datos en conversiones

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,6 +1,66 @@
-// Si bien todavía no tiene aplicación en la web, dejo la parte de objetos construido utilizando como ejemplo las monedas y sus conversiones
 
-let monedas = [];
+let pesosInput = document.getElementById("pesos")
+
+let moneda = document.getElementById("moneda")
+
+let boton = document.getElementById("btnprincipal")
+
+boton.addEventListener("click", respuestaClick)
+
+/*function respuestaClick(){
+    window.alert("$" + pesosInput.value + " a dólares son $" + parseInt(conversiones(pesosInput.value)) + " " + moneda.value + "s")
+}*/
+
+function respuestaClick(){
+    console.log("Entro chamigo")
+    Swal.fire("$" + pesosInput.value + " a dólares son $" + parseInt(conversiones(pesosInput.value)) + " " + moneda.value + "s")
+}
+
+
+/*function respuestaClick(){
+    Swal.fire(
+        "$" + pesosInput.value + " a dólares son ",
+        "$" + parseInt(conversiones(pesosInput.value)) + " " + moneda.value + "s"
+      )
+}*/
+
+
+
+
+
+function conversiones(dinero){
+    dinero = parseInt(dinero);
+    let conversionFinal = 0;
+    conversionFinal = parseInt(conversionFinal);
+
+    let monedaCambio = moneda.value;
+
+    switch (monedaCambio){
+        case "dolar":
+            conversionFinal = Math.trunc(pesosInput.value/440);
+            break;
+        case "euro":
+            conversionFinal = Math.trunc(pesosInput.value/225);
+            break;    
+        case "real":
+            conversionFinal = Math.trunc(pesosInput.value*0.024);
+            break;            
+        case "puruguayo":
+            conversionFinal = Math.trunc(pesosInput.value*0.19);
+            break; 
+    }
+    return conversionFinal;
+}
+
+
+
+
+
+// Por cambios en la implementación de la web lo siguiente ha quedado en desuso, lo dejo para que puedan evaluar la creacion de constructores y manejo de arreglos
+
+
+
+let arregloMonedas = [];
 
 //Contructor de monedas
 
@@ -11,10 +71,10 @@ function Moneda(nombre, precio) {
 
 
 function agregarMoneda(moneda){
-    monedas.push(moneda);
+    arregloMonedas.push(moneda);
 }
 
-let dolar = new Moneda  ("dolar", 400);
+let dolar = new Moneda  ("dolar", 440);
 agregarMoneda(dolar);
 let euro = new Moneda  ("euro", 225);
 agregarMoneda(euro);
@@ -26,16 +86,9 @@ agregarMoneda(uru);
 //Busqueda de una moneda
 
 function buscarMoneda(nombre){
-    for (var i=0; i<monedas.length;i++){
+    for (var i=0; i<arregloMonedas.length;i++){
         if(nombre == monedas[i].nombre){
             return monedas[i];
         }
     }
 }
-
-
-
-console.log(monedas)
-
-console.log(buscarMoneda(dolar))
-
